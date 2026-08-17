3e3500b529c940e66730df563b78f9b40c937d35
Nozzlenaut <125603517+nozzlenaut@users.noreply.github.com>
2026-08-17 09:16:24 -0400

    Rename reusable Short composition

--- video-generator/src/index.tsx
+++ video-generator/src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {Composition, registerRoot} from "remotion";
-import {A770Short} from "./short";
+import {PriceSiftShort} from "./short";
 import {totalFrames} from "./generated";
 
 const Root: React.FC = () => {
   return (
     <Composition
-      id="A770Short"
-      component={A770Short}
+      id="PriceSiftShort"
+      component={PriceSiftShort}
       durationInFrames={Math.max(1, totalFrames)}
       fps={30}
       width={1080}
